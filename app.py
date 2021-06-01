092c6b4fcf399d5c958bf2914bb63ba6e50356f7
johnvspeter <johnvspeter225@gmail.com>
2021-06-01 09:32:51 +0800

    修改Buttons template

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 def callback():
 
     if request.method == "GET":
-        return "打卡圖片修改"
+        return "修改Buttons template"
     if request.method == "POST":
         signature = request.headers["X-Line-Signature"]
         body = request.get_data(as_text=True)
@@ -48,9 +48,9 @@ def handle_message(event):
         buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
             template=ButtonsTemplate(
-                thumbnail_image_url='https://img.zcool.cn/community/01cf825b99c66fa8012099c8f44c32.png@1280w_1l_2o_100sh.png',
-                title='Menu',
-                text='Please select',
+                thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_IOaY-aGjlpLBUtM1xGqn74X-UIpiHH1ig&usqp=CAU',
+                title='還不快打卡',
+                text='想想你的一家老小阿',
                 actions=[
                     # PostbackAction(
                     #     label='postback',
