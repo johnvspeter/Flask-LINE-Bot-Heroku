71d9cbb08e5d341a1927f3ac8dbfb2b310ffb217
johnvspeter <johnvspeter225@gmail.com>
2021-06-01 15:45:36 +0800

    改成1分鐘

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
 def scheduled_job():
     url = " https://chichunbot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
