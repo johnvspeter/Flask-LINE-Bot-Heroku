025e94cd35073bf304dded9e966efbf873949d33
johnvspeter <johnvspeter225@gmail.com>
2021-06-02 11:12:50 +0800

    sched.add_job

--- clock.py
+++ clock.py
@@ -4,23 +4,22 @@ import urllib.request
 
 sched = BlockingScheduler()
 
-
-##@sched.scheduled_job('cron', day_of_week='mon-thu', hour='7,17',minute='15')
-@sched.scheduled_job('cron', day_of_week='mon-thu',minute='*/3')
+## 利用clock.py call回自己app的url來叫醒app
+@sched.scheduled_job('cron', day_of_week='mon-thu', hour='7,17',minute='15')
 def scheduled_job():
-    url = " https://chichunbot.herokuapp.com/card"
+    url = " https://chichunbot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
         
     for key, value in conn.getheaders():
         print(key, value)
-
-## 利用clock.py call回自己app的url來叫醒app
-@sched.scheduled_job('cron', day_of_week='mon-thu',minute='*/1')
-def scheduled_job2():
-    url2 = " https://chichunbot.herokuapp.com/"
+def job_task(): 
+    url2 = " https://chichunbot.herokuapp.com/card"
     conn2 = urllib.request.urlopen(url2)
         
     for key, value in conn2.getheaders():
         print(key, value)
+
+sched.add_job(job_task, 'interval', minute=1)
+
 
 sched.start()
