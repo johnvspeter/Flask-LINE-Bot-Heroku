356cb3a4d54f2bd467147937171e14f184098a07
johnvspeter <johnvspeter225@gmail.com>
2021-06-02 10:35:18 +0800

    url2

--- clock.py
+++ clock.py
@@ -15,12 +15,12 @@ def scheduled_job():
         print(key, value)
 
 ## 利用clock.py call回自己app的url來叫醒app
-@sched.scheduled_job('cron', day_of_week='mon-thu',minute='*/1')
-def scheduled_job():
-    url = " https://chichunbot.herokuapp.com/"
-    conn = urllib.request.urlopen(url)
+@sched.scheduled_job2('cron', day_of_week='mon-thu',minute='*/1')
+def scheduled_job2():
+    url2 = " https://chichunbot.herokuapp.com/"
+    conn2 = urllib.request.urlopen(url2)
         
-    for key, value in conn.getheaders():
+    for key, value in conn2.getheaders():
         print(key, value)
 
 sched.start()
