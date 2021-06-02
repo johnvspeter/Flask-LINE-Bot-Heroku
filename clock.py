bbc25a3b6343ad1575f43c76ede10419e169f440
johnvspeter <johnvspeter225@gmail.com>
2021-06-02 11:19:23 +0800

    minute

--- clock.py
+++ clock.py
@@ -4,6 +4,12 @@ import urllib.request
 
 sched = BlockingScheduler()
 
+def job_task(): 
+    url2 = " https://chichunbot.herokuapp.com/card"
+    conn2 = urllib.request.urlopen(url2)
+        
+    for key, value in conn2.getheaders():
+        print(key, value)
 ## 利用clock.py call回自己app的url來叫醒app
 @sched.scheduled_job('cron', day_of_week='mon-thu', hour='7,17',minute='15')
 def scheduled_job():
@@ -12,14 +18,9 @@ def scheduled_job():
         
     for key, value in conn.getheaders():
         print(key, value)
-def job_task(): 
-    url2 = " https://chichunbot.herokuapp.com/card"
-    conn2 = urllib.request.urlopen(url2)
-        
-    for key, value in conn2.getheaders():
-        print(key, value)
 
-sched.add_job(job_task, 'interval', minute=1)
+
+sched.add_job(job_task, 'interval', minute='1')
 
 
 sched.start()
