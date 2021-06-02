5c4a6b21ebb295ddd8015eb3403b124a73a8a92e
johnvspeter <johnvspeter225@gmail.com>
2021-06-02 11:47:18 +0800

    '*/5'

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ import urllib.request
 sched = BlockingScheduler()
 
 ## 利用clock.py call回自己app的url來叫醒app
-@sched.scheduled_job('cron', day_of_week='mon-thu', minute='1')
+@sched.scheduled_job('cron', day_of_week='mon-thu', minute='*/1')
 def scheduled_job():
     url = " https://chichunbot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
@@ -13,7 +13,7 @@ def scheduled_job():
     for key, value in conn.getheaders():
         print(key, value)
 
-@sched.scheduled_job('cron', day_of_week='mon-thu', minute='1')
+@sched.scheduled_job('cron', day_of_week='mon-thu', minute='*/1')
 def scheduled_job2():
     url = " https://chichunbot.herokuapp.com/card"
     conn = urllib.request.urlopen(url)
